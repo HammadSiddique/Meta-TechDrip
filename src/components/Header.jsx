8f12069737e425f22a120a804cc9c48273a077c2
HammadSiddique <hammad.siddique01@gmail.com>
2022-07-03 01:14:34 +0500

    Removed commented code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,8 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-// import { useDispatch } from 'react-redux';
 import { FaMicrophone } from 'react-icons/fa';
 import { AiOutlineSetting } from 'react-icons/ai';
 import { MdOutlineArrowBackIosNew } from 'react-icons/md';
-// import { BsSearch } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
-// import { fetchProducts } from '../redux/products/products';
 
 const Header = () => (
   <header>
@@ -18,23 +14,6 @@ const Header = () => (
         <i><AiOutlineSetting /></i>
       </div>
     </nav>
-    {/* <div className="search-bar">
-        <form onSubmit={submitHandler}>
-          <input
-            type="text"
-            value={search}
-            placeholder="Search Products"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button type="submit"><BsSearch /></button>
-        </form>
-      </div> */}
   </header>
 );
 export default Header;
-// const [search, setSearch] = useState('');
-// const dispatch = useDispatch();
-// const submitHandler = (e) => {
-//   e.preventDefault();
-//   dispatch(fetchProducts(search));
-// };
